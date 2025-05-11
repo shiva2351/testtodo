9366b68d26ecdbdc4bb31d1b68567b095862f2a0
shiva <smpgr2351@gmail.com>
2025-05-11 09:36:56 +0530

    first

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+const path = require("path");
+const dbpath = path.join(__dirname, "todo.db");
+
+let db = null;
+const initilizeDBandServer = async () => {
+  try {
+    db = await open({ filename: dbpath, driver: sqlite3.Database });
+    app.listen(3000, () => {
+      console.log("Server is running at http://localhost:3000");
+    });
+  } catch (e) {
+    console.log(`DB Error : ${e.message}`);
+    process.exit(1);
+  }
+};
+initilizeDBandServer();
+
+app.get('/tasks', async(req, res) => {
+    console.log("get1")
+    const getQuery = `SELECT * FROM todo;`;
+
+  const Res = await db.all(getQuery);
+  console.log(getQuery,Res);
+  //response.send(Res);
+  res.send(Res);
+    res.json(res);
+});
+
+app.get('/t', async(req, res) => {
+    
+  console.log("getQuery");
+    res.json("ff");
+});
+
+
+
+
+
+
+
